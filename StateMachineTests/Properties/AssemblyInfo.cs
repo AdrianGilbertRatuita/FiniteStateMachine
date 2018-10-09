9a6008cd59eaddb14eeefe2fc747f03a84eece85
Adrian Gilbert Ratuita <adrianratuita@gmail.com>
2018-10-08 21:51:54 -0500

    UnitTesting Solution
    Created a UnitTesting Solution to prepare UnitTesting the StateMachine

---
+++ StateMachineTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StateMachineTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StateMachineTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff7d8530-bac7-46ce-8ec9-765cab971cf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
